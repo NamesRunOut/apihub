02dc1b2f9a69fa6a3af085d6297c8a68dd8a055f
NamesRunOut <ewawozny4@gmail.com>
2020-08-05 19:16:29 +0200

    123

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        text: "",
+        error: "error"
+      }
+
+    changeScript = () => {
+        const value = this.refs.in1.value;
+        fetch(`http://numbersapi.com/${value}?json`)
+        .then(response => {
+            if (response.ok) return response;
+            else throw Error('error fetching data');
+        })
+        .then(response => response.json())
+        .then(data => this.setState({
+            text: data.text
+        }))
+        .catch(error => this.setState({ text: 'error fetching data' }))
+    }
+
+    render() {
+      return (
+        <div class="wrapper">
+        <div>
+          Henlo
+          <input onChange={this.changeScript} type="text" ref="in1"></input>
+        </div>
+        <div id="response">
+            {this.state.text}
+        </div>
+        </div>
+      );
+    }
 }
+
 
 export default App;
