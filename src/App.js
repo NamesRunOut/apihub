58d232f69d4fa2215817ced8dfc51284529b7f7c
NamesRunOut <ewawozny4@gmail.com>
2020-08-11 15:30:00 +0200

    123

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ class App extends Component {
 
     state = {
         text: "",
+        norris: "",
+        bored: "",
+        nameday: "",
         error: "error"
       }
 
@@ -22,16 +25,67 @@ class App extends Component {
         .catch(error => this.setState({ text: 'error fetching data' }))
     }
 
+        changeScriptNorris = () => {
+        fetch(`http://api.chucknorris.io/jokes/random`)
+        .then(response => response.json())
+        .then(data => this.setState({
+            norris: data.value
+        }))
+        .catch(error => this.setState({ text: 'error fetching data' }))
+        }
+
+                changeScriptBored = () => {
+                fetch(`https://www.boredapi.com/api/activity`)
+                .then(response => response.json())
+                .then(data => this.setState({
+                    bored: data.activity
+                }))
+                .catch(error => this.setState({ text: 'error fetching data' }))
+                }
+
+            changeScriptNameday = () => {
+                fetch(`https://api.abalin.net/today?country=pl`)
+                .then(response => response.json())
+                .then(data => this.setState({
+                    nameday: data.pl
+                }))
+                .catch(error => this.setState({ text: 'error fetching data' }))
+                }
+
     render() {
       return (
         <div class="wrapper">
-        <div>
-          Henlo
-          <input onChange={this.changeScript} type="text" ref="in1"></input>
-        </div>
-        <div id="response">
-            {this.state.text}
-        </div>
+          <div class="api-numbers">
+               numbers api
+               <input onChange={this.changeScript} type="text" ref="in1"></input>
+               <div>
+                 {this.state.text}
+                </div>
+          </div>
+
+          <div class="api-norris">
+              chuck norris api
+              <input onChange={this.changeScriptNorris} type="text"></input>
+               <div>
+                 {this.state.norris}
+              </div>
+           </div>
+
+           <div class="api-bored">
+            bored api
+                  <input onChange={this.changeScriptBored} type="text"></input>
+                  <div>
+                 {this.state.bored}
+               </div>
+           </div>
+
+<div class="api-nameday">
+            nameday api
+                  <input onChange={this.changeScriptNameday} type="text"></input>
+                  <div>
+                 {this.state.bored}
+               </div>
+           </div>
         </div>
       );
     }
