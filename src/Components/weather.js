c422819158ade897ec4ab38da379d04348881c1f
namesrunout <namesrunout@gmail.com>
2020-08-20 17:27:09 +0200

    build

--- src/Components/weather.js
+++ src/Components/weather.js
@@ -22,7 +22,7 @@ import windD from './icons/windD.svg'
 import wind_cloudD from './icons/wind_cloudD.svg'
 import thunder_rainD from './icons/thunder_rainD.svg'
 
-const WEATHER_KEY = process.env.OPENWEATHER
+const WEATHER_KEY = process.env.REACT_APP_AUTH_TOKEN;
 
 class Weather extends React.Component{
 
